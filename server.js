0221d562ea498068f322fd25c22c16c629fcdeba
Gabriel De Marco <biel_dmarco@hotmail.com>
2020-11-16 11:10:04 -0300

    add server.js to deploy

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const compression = require('compression');
+
+const port = process.env.PORT || 4000;
+const app = express();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(compression());
+}
+
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port);
