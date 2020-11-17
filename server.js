cdf0773f2e655fc058f3ac0d546a54f3aef03f61
Gabriel De Marco <biel_dmarco@hotmail.com>
2020-11-16 22:23:28 -0300

    update server.js

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const path = require('path');
+const enforce = require('express-sslify');
 const compression = require('compression');
 
 const port = process.env.PORT || 4000;
 const app = express();
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(compression());
 }
 
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', (req, res) => {
+app.get('/*', (_, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
