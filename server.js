85e295f971d5560e96daf96597ee2cff735ba820
Gabriel De Marco <biel_dmarco@hotmail.com>
2021-11-18 14:16:53 -0300

    version mvp-0.0.3

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const enforce = require('express-sslify');
 const compression = require('compression');
+const cors = require('cors');
 
 const port = process.env.PORT || 4000;
 const app = express();
@@ -11,6 +13,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(compression());
 }
 
+app.use(cors());
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -18,4 +21,6 @@ app.get('/*', (_, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port);
+app.listen(port, () => {
+  console.log('Server is up!');
+});
